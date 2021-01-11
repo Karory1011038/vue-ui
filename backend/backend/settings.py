5aa93762664cb12169319ff8ad97752a06eca105
Karory1011038 <edikaleksandrov4309@gmail.com>
2021-01-12 00:07:43 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '..\dist\static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../dist/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../dist/static/vue')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

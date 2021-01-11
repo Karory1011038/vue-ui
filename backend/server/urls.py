92b00f34956a5c7aeb18c9086526bfabeaa4236a
Karory1011038 <edikaleksandrov4309@gmail.com>
2021-01-11 23:52:40 +0300

    fix

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -5,7 +5,7 @@ from .views import index_view
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + [
     path('send/', views.send_messange, name='send_messange'),
     path('', index_view, name='index'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]

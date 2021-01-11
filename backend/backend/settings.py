2f5a962713016c9aad9c0108582f43bcc24dd12d
Karory1011038 <edikaleksandrov4309@gmail.com>
2021-01-11 23:10:42 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+sys.path.append(BASE_DIR)
 
 # Application definition
 
@@ -38,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'backend.server'
+    'server'
 ]
 
 MIDDLEWARE = [

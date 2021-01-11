8eb1dc3d9e6d3875ea22e7ba8748780f652be246
Karory1011038 <edikaleksandrov4309@gmail.com>
2021-01-12 00:35:01 +0300

    fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('', include('server.urls')),
     path('admin/', admin.site.urls),
+    path('', include('server.urls')),
 ]
